095d44d3414648e34eff9371d3c7ab7a324d7184
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-11-30 19:06:38 +0530

    xx
    dxd

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/global.css'
+import './index.css'
 import './styles/variables.css'
+import './styles/global.css'
 import App from './App.jsx'
 import { WorkflowProvider } from './context/WorkflowContext.jsx'
 
